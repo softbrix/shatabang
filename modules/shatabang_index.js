b8d9604cd5c933605bbbe568846eab4c36f1391d
Andreas Sehr <andreas@mansehr.se>
2016-04-29 02:51:11 +0200

    Updated the index to be in memory

--- modules/shatabang_index.js
+++ modules/shatabang_index.js
@@ -1,58 +1,22 @@
-
+"use strict"
 /**
 This file describes the index module for the keyword store and lookup
 
 The index stores all information to the file system.
 */
-"use strict"
 
 var _ = require('underscore');
 var fs = require('fs-extra');
 var path = require('path');
-var jsonfile = require('jsonfile');
 var touch = require("touch");
+var shFiles = require("./shatabang_files");
+var StringSet = require("stringset");
 
-var pathToUse;
 const prefix = '_';
 const masterKeyFileName = prefix + prefix + 'allKeys';
 const valueDelimiter = '\n';
 const defaultEncoding = 'utf8';
-
-var getIndexPath = function() {
-  if(_.isUndefined(pathToUse)) {
-    throw new Error('The indexer has not been initialized with a path. Call usePath.');
-  }
-  return pathToUse;
-};
-
-/*var dirStructure = {
-  1 : [prefix, 1],
-  3 : [1, prefix, 2],
-  5 : [1, 2, prefix, 5]
-};*/
-
-var getFilePathFromKey = function(key) {
-  // Replace slashes with underscore
-  key = key.replace(/\\|\//g, "_");
-  var fileName;
-  if(key.length < 3) {
-    fileName = prefix + key;
-  } else if(key.length < 5) {
-    var index = 2;
-    fileName = path.join(key.substring(0, index), prefix + key.substring(index));
-  } else {
-    var index1 = 2;
-    var index2 = 4;
-    fileName = path.join(key.substring(0, index1), key.substring(index1, index2), key.substring(index2));
-  }
-  return path.join(getIndexPath(), fileName);
-};
-
-var getMasterKeyFile = function() {
-  var fileName = path.join(getIndexPath(), masterKeyFileName);
-  touchFile(fileName);
-  return fileName;
-};
+const chars='abcdefghijklmnopqrstuvwxyz';
 
 var touchFile = function(filepath) {
   var dirpath = path.dirname(filepath);
@@ -64,61 +28,151 @@ var touchFile = function(filepath) {
   touch.sync(filepath);
 };
 
-var putToFile = function(fileName, value) {
+
+var getIdFromKey = function(key) {
+  return ((parseInt(key, 36) * 2) % chars.length) + 1;
+};
+
+var putToFile = function(fileName, value, key) {
+  if(_.isUndefined(key)) {
+    key = "";
+  }
   var fileContent = fs.readFileSync(fileName, defaultEncoding) || "";
   if(fileContent.indexOf(value) === -1) {
     if(fileContent.length > 0) {
-      value = valueDelimiter + value;
+      value = valueDelimiter + key + '_' + value;
     }
     fs.appendFileSync(fileName, value, defaultEncoding);
   }
 };
 
 var readFile = function(fileName) {
+  //console.log('Loads file: ' + fileName);
   try {
-    return (fs.readFileSync(fileName, defaultEncoding) || "").split(valueDelimiter);
+    var idx = {};
+    var data = fs.readFileSync(fileName, defaultEncoding);
+    if(_.isEmpty(data)) {
+      return idx;
+    }
+    var parsed = JSON.parse(data);
+    _.each(parsed, function(list, key) {
+      list.forEach(function(value) {
+        addValue(idx, key, value);
+      });
+    });
+    return idx;
   } catch(err) {
     //console.log(err);
-    return [];
+    return {};
   }
 };
 
-module.exports = {
-  usePath : function(newPathToUse) {
-    pathToUse = newPathToUse;
-  },
-  /**
-  Expects a key string and value string as parameters. These will be added to
-  the internal index
-  */
-  put : function(key, value) {
-    if(!_.isString(key) || !_.isString(value) ||
-      key.length === 0 || value.length === 0) {
-      return;
-    }
-    var fileName = getFilePathFromKey(key);
-
-    touchFile(fileName);
-
-    putToFile(fileName, value);
-
-    var keyFile = getMasterKeyFile();
-    putToFile(keyFile, key);
-  },
-  /**
-   Return all items for the matching key
-   */
-  get : function(key) {
-    if(!_.isString(key) || key.length === 0 ) {
-      return [];
-    }
-
-    var fileName = getFilePathFromKey(key);
-
-    return readFile(fileName);
-  },
-  search : function(searchStr) {
-    var keys = readFile(getMasterKeyFile());
-    return _.filter(keys, function(item){ return item.indexOf(searchStr) >= 0; });
+var addValue = function(idx, key, value) {
+  if(_.isUndefined(idx[key])) {
+    idx[key] = new StringSet();
   }
+  idx[key].add(value);
+};
+
+module.exports = function(pathToUse) {
+  var getMasterKeyFile = function() {
+    var fileName = path.join(pathToUse, masterKeyFileName);
+    touchFile(fileName);
+    return fileName;
+  };
+
+  var _idx = [new StringSet()];
+
+  // Load old master key file
+  var data = fs.readFileSync(getMasterKeyFile(), defaultEncoding);
+  if(!_.isEmpty(data)) {
+    var parsed = JSON.parse(data);
+    parsed.forEach(function(itm) {
+      _idx[0].add(itm);
+    });
+  }
+
+  var getIndexFromKey = function(key) {
+    var i = getIdFromKey(key);
+    if(_.isUndefined(_idx[i])) {
+      _idx[i] = readFile(getFileFromIndex(i));
+    }
+    return _idx[i];
+  };
+
+  var getFileFromIndex = function(idx) {
+    return path.join(pathToUse, chars[idx-1]);
+  };
+
+
+
+  var writeFile = function(list, fileName) {
+    //console.log(list);
+    shFiles.writeFile(fileName, JSON.stringify(list), function(err) {
+      if(err) {
+        return;
+      }
+      console.log("The file was saved: ", fileName);
+    });
+  };
+
+  var getIndexAsList = function(index) {
+    var list = {};
+    _.each(index, function(keySet, key) {
+      this[key] = keySet.items();
+    }, list);
+    return list;
+  };
+
+  var flush = function() {
+    writeFile(_idx[0].items(), getMasterKeyFile());
+    _.times(chars.length, function(i) {
+      ++i;
+      if(_.isEmpty(_idx[i])) {
+        return;
+      }
+      writeFile(getIndexAsList(_idx[i]), getFileFromIndex(i));
+    });
+  };
+
+  var throttled_flush = _.throttle(flush, 5000);
+
+  return {
+    /**
+    Expects a key string and value string as parameters. These will be added to
+    the internal index
+    */
+    put : function(key, value) {
+      if(!_.isString(key) || !_.isString(value) ||
+        key.length === 0 || value.length === 0) {
+        return;
+      }
+      var idx = getIndexFromKey(key);
+
+      addValue(idx, key, value);
+
+      _idx[0].add(key);
+
+      throttled_flush();
+    },
+    /**
+     Return all items for the matching key
+     */
+    get : function(key) {
+      if(!_.isString(key) || key.length === 0 ) {
+        return [];
+      }
+
+      var idx = getIndexFromKey(key);
+
+      if(_.isUndefined(idx[key])) {
+        return [];
+      }
+      return idx[key].items();
+    },
+    search : function(searchStr) {
+      var keys = _idx[0].items();
+      return _.filter(keys, function(item){ return item.indexOf(searchStr) >= 0; });
+    }
+  };
 };
