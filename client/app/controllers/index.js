862edd572e8d5240d4844a77e965e81fc57f537b
Andreas Sehr <andreas@mansehr.se>
2017-12-29 14:26:52 +0100

    WIP: goto date in client

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -6,16 +6,34 @@ export default Controller.extend({
   imageWidthService: service('image-width'),
   mediaLoader: service('media-list-loader'),
 
+  fromDate: '1970-01-01',
+  toDate: '1979-09-09',
+
   init() {
     this._super(...arguments);
     // Only init if we are authenticated...
     if (this.get('session.isAuthenticated')) {
-      this.get('mediaLoader');
+      var now = new Date();
+
+      this.set('toDate', now.toLocaleDateString());
+      this.set('fromDate', now.toLocaleDateString());
+      var tree = this.get('mediaLoader.tree');
+
+      this.get('mediaLoader').fullyLoadedPromise().then(() => {
+        var it = tree.leafIterator();
+        var d = now;
+        if(it.hasNext()) {
+          d = it.next().date;
+        }
+        this.set('fromDate', d.toLocaleDateString());
+      });
     }
     this.get('imageWidthService');
   },
   actions: {
     zoomIn: function() { this.get('imageWidthService').zoomIn(); },
-    zoomOut: function() { this.get('imageWidthService').zoomOut(); }
+    zoomOut: function() { this.get('imageWidthService').zoomOut(); },
+    fromDateChanged: function(e) { console.log(e.target.value); },
+    toDateChanged: function(e) { console.log(e.target.value); }
   }
 });
--- task_processors/encode_video.js
+++ task_processors/encode_video.js
@@ -7,6 +7,7 @@ var fs = require('fs-extra');
 
 var init = function(config, task_queue) {
   var storageDir = config.storageDir, cacheDir = config.cacheDir;
+  const MAX_WIDTH = 1920, MAX_HEIGHT = 1080;
 
   task_queue.registerTaskProcessor('encode_video', function(data, job, done) {
     var file = path.parse(data.file);
@@ -14,29 +15,25 @@ var init = function(config, task_queue) {
     var outputFileName = path.join(cacheDir, '1920', file.dir, file.name + '.mp4'),
         sourceFileName = path.join(storageDir, file.dir, file.base);
 
-      console.log(sourceFileName, outputFileName);
       fs.mkdirsSync(path.dirname(outputFileName));
-
-      //ffmpeg -i data/sorterat/2006/04/25/094601.avi -y -c:v libx264  -c:a aac -pix_fmt yuv420p -movflags faststart data/cache/1920/2006/04/25/094601.mp4
-      //ffmpeg -movflags faststart -pix_fmt yuv420p -i data/sorterat/2006/04/25/102753.avi -y -acodec aac -vcodec libx264 data/cache/1920/2006/04/25/102753.mp4
 
       ffmpeg(sourceFileName, {logger: console})
         .videoCodec('libx264')
         .audioCodec('aac')
         .outputOptions([
           '-movflags faststart',
-          //'-c:v libx264',
           '-pix_fmt yuv420p',
-          //'-c:a aac'
+          '-hide_banner',
+          '-preset slower'
         ])
         //.size('640x480')
         .on('error', function(err) {
           console.log('Error encoding file', err);
           done(err);
         })
-        /*.on('progress', function(progress) {
+        .on('progress', function(progress) {
           job.progress(progress.percent, 100);
-        })*/
+        })
         .on('end', function() {
           done();
         })
