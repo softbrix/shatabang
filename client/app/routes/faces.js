aaa71a38cfe2172e5340485882b870ed42a294b7
Andreas Sehr <andreas@mansehr.se>
2018-02-05 13:58:53 +0100

    Show the largest faces first.

--- client/app/routes/faces.js
+++ client/app/routes/faces.js
@@ -1,8 +1,57 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+
+// TODO: Remove code dupplication
+// This could be handled on the server
+const MAX_SHORT = 65535;
+
+function fromHex(v) {
+  return parseInt(v, 16);
+}
+
+function expandFaceInfo(info) {
+  if(info.length < 14 /* todo: regexp match input*/) {
+    return { x: NaN, y: NaN, w: NaN, h: NaN };
+  }
+  var t = function t(val) {
+    return fromHex(val) / MAX_SHORT;
+  };
+  const BLK_WIDTH = 4;
+  return {
+    x: t(info.substr(0, BLK_WIDTH)),
+    y: t(info.substr(4, BLK_WIDTH)),
+    w: t(info.substr(8, BLK_WIDTH)),
+    h: t(info.substr(12, BLK_WIDTH))
+  };
+}
+// END TODO
+
 export default Route.extend(AuthenticatedRouteMixin).extend({
   model() {
-    return Ember.$.getJSON('./api/faces/list').then(list => list.slice(0, 512));
+    return Ember.$.getJSON('./api/faces/list')
+    .then(function(facesJson) {
+      var faceItems = [];
+      facesJson.forEach(function(itm) {
+        itm.items.forEach(function(face) {
+          face.k = itm.key;
+          faceItems.push(face);
+        })
+      });
+      return faceItems;
+    })
+    .then(list =>  {
+      list.forEach((a) => {
+        if(a.s * 1 !== a.s) {
+          Ember.log('No stat', a.k);
+          a.s = 0;
+        }
+      });
+      return list;
+    })
+    .then(list => list.map(itm => { var o = expandFaceInfo(itm.i); o.s = itm.s; o.k = itm.k; o.b = itm.b; return o}))
+    .then(list => list.map(itm => { itm.a = itm.h * itm.w; return itm; }))
+    .then(list => list.sort((a,b) => b.a - a.a))  // Sort desc based on focus
+    .then(l => l.slice(0, 1024));
   }
 });
