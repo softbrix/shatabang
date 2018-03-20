0a8649ee4c68724b978ae1ed863219c3810566bf
Andreas Sehr <andreas@mansehr.se>
2018-03-20 23:26:09 +0100

    Bugfix: Upgrade check should set new version value

--- task_processors/upgrade.js
+++ task_processors/upgrade.js
@@ -37,13 +37,14 @@ var init = function(config, task_queue) {
       if(version < 3) {
         upgrade_faces_index(infoDirectory, config.cacheDir, task_queue);
         task_queue.retryFailed();
+      }
+      if(version < latestVersion) {
+        import_meta_to_index(infoDirectory, config.cacheDir, task_queue);
+
         job.log('Successfully upgraded index to', 'v'+latestVersion);
         redis.set(versionKey, latestVersion, function() {
           done();
         });
-      }
-      if(version < latestVersion) {
-        import_meta_to_index(infoDirectory, config.cacheDir, task_queue);
       } else {
         job.log('All done');
         done();
